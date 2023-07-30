b6577bc3d0a9a6765def1e8045ce03d84e7b5d39
rawatrk <ritesh@onetick.in>
2023-07-30 14:10:38 +0530

    Day-5: 1-Uses SingleChildScrollView to avoid pixels problem. 2-Created Route class and used Navigator to move one screen to other screen. 3-modified login button size.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/routes.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
@@ -7,46 +9,48 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
-          SizedBox(height: 20.0,),
-          Text('Weclome', style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold
-          ),),
-          SizedBox(height: 20.0,),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter username',
-                    labelText: 'Username'
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
+            SizedBox(height: 28.0,),
+            Text('Welcome', style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold
+            ),),
+            SizedBox(height: 20.0,),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter username',
+                      labelText: 'Username'
+                    ),
                   ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: 'Enter password',
-                      labelText: 'Password'
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Enter password',
+                        labelText: 'Password'
+                    ),
                   ),
-                ),
-                SizedBox(height: 20.0,),
-                ElevatedButton(
-                  child: Text('Login'),
-                  style: TextButton.styleFrom(
-                    // backgroundColor: Colors.deepPurpleAccent,
-                  ),
-                  onPressed: (){
-                    print('Login page');
-                  },
-                )
-              ],
+                  SizedBox(height: 30.0,),
+                  ElevatedButton(
+                    child: Text('Login'),
+                    style: TextButton.styleFrom(
+                      minimumSize: Size(300, 45)
+                    ),
+                    onPressed: (){
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
